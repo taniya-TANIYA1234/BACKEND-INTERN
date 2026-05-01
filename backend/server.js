24d67eeaa6efc214bb85afdf34cb69e57e64e0e8
Taniya Chauhan <Taniyachauhan012345@gmail.com>
2026-05-01 15:11:31 +0530

    deploy process

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ async function main() {
     await mongoose.connect(process.env.MONGO_URL);
 }
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use( (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 
